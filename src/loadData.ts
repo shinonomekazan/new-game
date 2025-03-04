52a57af96b4fca47f33175741b618419f7b6fbb7
lethanh1440 <126956397+lethanh1440@users.noreply.github.com>
2025-03-04 08:51:39 +0700

    Fix/read content (#5)
    * load ggsheet api
    
    * load with format
    
    ---------
    
    Co-authored-by: thanh <a>

--- src/loadData.ts
+++ src/loadData.ts
@@ -5,12 +5,12 @@ export class loadData extends BaseStep {
 	public onStep(eventName: FlowEventName) {
 		switch (eventName) {
 			case FlowEventName.GameLoad:
-				let scene = g.game.scene();
-				let t = scene.asset.getText('/assets/data.xml').data;
-				var parse = new DOMParser();
-				let xmlDoc = parse.parseFromString(t, "text/xml")
-				const root = xmlDoc.documentElement;
-				setSender(root)
+				//let scene = g.game.scene();
+				//let t = scene.asset.getText('/assets/data.xml').data;
+				//var parse = new DOMParser();
+				//let xmlDoc = parse.parseFromString(t, "text/xml")
+				//const root = xmlDoc.documentElement;
+				//setSender(root)
 				this.runNext();
 				break;
 		}
